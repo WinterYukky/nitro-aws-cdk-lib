ab4385f443a85b9c9a53e1cf9bf97a5e7c947d13
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2022-07-09 04:16:13 +0000

    test: debug on GitHub Actions

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,8 @@
 import { readdirSync } from "fs";
 import { App, Stack } from "aws-cdk-lib";
-import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
-import { Bucket } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment } from "aws-cdk-lib/aws-s3-deployment";
+// import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+// import { Bucket } from "aws-cdk-lib/aws-s3";
+// import { BucketDeployment } from "aws-cdk-lib/aws-s3-deployment";
 import { NitroAsset } from "../src";
 
 describe("Another directory", () => {
@@ -30,53 +30,53 @@ describe("Another directory", () => {
   }
 });
 
-describe("Server only", () => {
-  const app = new App({
-    outdir: process.env.CI === "true" ? "cdk.out/" : undefined,
-  });
-  const stack = new Stack(app, "TestStack");
-  const asset = new NitroAsset(stack, "ServerOnly", {
-    path: "test/data/server-only",
-  });
-  it("files are should empty", () => {
-    expect(asset.staticAsset.files).toEqual([]);
-  });
-  it("directories are should empty", () => {
-    expect(asset.staticAsset.directories).toEqual([]);
-  });
-  it("should create dotfile", () => {
-    const files = readdirSync(asset.staticAsset.path);
-    expect(files).toEqual(["dotfile"]);
-  });
-});
+// describe("Server only", () => {
+//   const app = new App({
+//     outdir: process.env.CI === "true" ? "cdk.out/" : undefined,
+//   });
+//   const stack = new Stack(app, "TestStack");
+//   const asset = new NitroAsset(stack, "ServerOnly", {
+//     path: "test/data/server-only",
+//   });
+//   it("files are should empty", () => {
+//     expect(asset.staticAsset.files).toEqual([]);
+//   });
+//   it("directories are should empty", () => {
+//     expect(asset.staticAsset.directories).toEqual([]);
+//   });
+//   it("should create dotfile", () => {
+//     const files = readdirSync(asset.staticAsset.path);
+//     expect(files).toEqual(["dotfile"]);
+//   });
+// });
 
-describe("NitroStaticAsset.resolveCloudFrontBehaviors", () => {
-  const app = new App({
-    outdir: process.env.CI === "true" ? "cdk.out/" : undefined,
-  });
-  const stack = new Stack(app, "TestStack");
-  const asset = new NitroAsset(stack, "ResolveCloudFrontBehaviors", {
-    path: "test/data/resolve-cloud-front-behaviors",
-  });
-  const bucket = new Bucket(stack, "Bucket");
-  const s3Origin = new S3Origin(bucket);
-  const additionalBehaviors = asset.staticAsset.resolveCloudFrontBehaviors({
-    resolve: () => ({
-      origin: s3Origin,
-    }),
-  });
-  new BucketDeployment(stack, "Deployment", {
-    sources: [asset.staticAsset],
-    destinationBucket: bucket,
-  });
-  it("files are as it", () => {
-    expect(additionalBehaviors).toMatchObject({
-      "favicon.ico": { origin: s3Origin },
-    });
-  });
-  it("directories are marked with an asterisk", () => {
-    expect(additionalBehaviors).toMatchObject({
-      "_nuxt/*": { origin: s3Origin },
-    });
-  });
-});
+// describe("NitroStaticAsset.resolveCloudFrontBehaviors", () => {
+//   const app = new App({
+//     outdir: process.env.CI === "true" ? "cdk.out/" : undefined,
+//   });
+//   const stack = new Stack(app, "TestStack");
+//   const asset = new NitroAsset(stack, "ResolveCloudFrontBehaviors", {
+//     path: "test/data/resolve-cloud-front-behaviors",
+//   });
+//   const bucket = new Bucket(stack, "Bucket");
+//   const s3Origin = new S3Origin(bucket);
+//   const additionalBehaviors = asset.staticAsset.resolveCloudFrontBehaviors({
+//     resolve: () => ({
+//       origin: s3Origin,
+//     }),
+//   });
+//   new BucketDeployment(stack, "Deployment", {
+//     sources: [asset.staticAsset],
+//     destinationBucket: bucket,
+//   });
+//   it("files are as it", () => {
+//     expect(additionalBehaviors).toMatchObject({
+//       "favicon.ico": { origin: s3Origin },
+//     });
+//   });
+//   it("directories are marked with an asterisk", () => {
+//     expect(additionalBehaviors).toMatchObject({
+//       "_nuxt/*": { origin: s3Origin },
+//     });
+//   });
+// });
